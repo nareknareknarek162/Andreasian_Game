1e99645d8cdd8214358a7cc61aceaf8969c3872b
gaa16 <narek7284@gmail.com>
2022-01-18 01:05:01 +0300

    добавлены спрайты

--- main.py
+++ main.py
@@ -7,24 +7,19 @@ size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 
 
-def load_image(name, colorkey=None):
+def load_image(name):
     fullname = os.path.join(r'C:/Users/gaa16/PycharmProjects/pythonProject1/Andreasian_Game', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
     return image
 
 if __name__ == '__main__':
-    image = load_image("img.png")
-    screen.blit(image, (10, 10))
+    im1 = load_image("brock.png")
+    im2 = load_image("wrock.png")
+    screen.blit(im1, (10, 10))
+    screen.blit(im2, (30, 45))
     running = True
     while running:
         for event in pygame.event.get():
